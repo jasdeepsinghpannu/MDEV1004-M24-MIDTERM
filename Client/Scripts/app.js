f5fd983fba228f4ecf50b407245f321d67c01d0a
jasdeepsingh <200540018@student.georgianc.on.ca>
2024-06-21 21:47:03 -0400

    Removed again :(

--- Client/Scripts/app.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(function () {
-    function Start() {
-        console.log("Application started... ");
-    }
-    window.addEventListener("load", Start);
-})();
-//# sourceMappingURL=app.js.map
--- Client/Scripts/app.ts
+++
@@ -1,8 +0,0 @@
-// IIFE -- Immediately Invoked Function Expression
-(function () {
-    function Start(): void
-    {
-        console.log("Application started... ")
-    }
-    window.addEventListener("load", Start);
-})();
