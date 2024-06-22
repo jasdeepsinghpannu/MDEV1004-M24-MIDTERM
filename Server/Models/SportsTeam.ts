b1ce5495cdbb6a31cc394fcbae21b95f3267ed2b
jasdeepsingh <200540018@student.georgianc.on.ca>
2024-06-21 22:10:02 -0400

    Created sports team schema

---
+++ Server/Models/SportsTeam.ts
@@ -0,0 +1,33 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+// SportsTeam Interface
+interface ISportsTeam {
+  teamName: string;
+  sportType: string;
+  coach: string;
+  captain: string;
+  players: string[];
+  homeVenue: string;
+  league: string;
+  championshipsWon: number;
+  foundedYear: number;
+  websiteURL: string;
+  logoURL: string;
+}
+
+// Sports Team Schema
+let sportsTeamSchema = new Schema<ISportsTeam>({
+  teamName: { type: String, required: true },
+  sportType: { type: String, required: true },
+  coach: { type: String, required: true },
+  captain: { type: String, required: true },
+  players: { type: [String], required: true },
+  homeVenue: { type: String, required: true },
+  league: { type: String, required: true },
+  championshipsWon: { type: Number, default: 0 },
+  foundedYear: { type: Number, required: true },
+  websiteURL: { type: String, required: true },
+  logoURL: { type: String, required: true },
+});
+
+export default mongoose.model<ISportsTeam>('SportsTeam', sportsTeamSchema);
